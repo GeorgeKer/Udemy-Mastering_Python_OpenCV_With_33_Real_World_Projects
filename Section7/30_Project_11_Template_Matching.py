f6f7a590970b32757d91a3923c131f00514555be
GeorgeKer <geokeratz@gmail.com>
2026-02-16 22:14:12 +0200

    Added Project 11 for Matching Templates

--- Section7/30_Project_11_Template_Matching.py
+++ Section7/30_Project_11_Template_Matching.py
@@ -1,16 +1,31 @@
 import cv2 as cv
-import numpy as np 
 import os
 import pytesseract
 
-img_path = os.path.expanduser('~/INPUT/TEST_IMAGE_WITH_TEXT.png')
-img = cv.imread(img_path)
-if img is None:
-    raise FileNotFoundError(f"Image not found at path: {img_path}")
+img_path = os.path.expanduser('~/INPUT/TEST_IMAGE.jpg')
+template_path = os.path.expanduser('~/INPUT/TEST_TEMPLATE_SHIP_CROP.png')
+img = cv.imread(img_path, 0)
+if img is None: raise FileNotFoundError(f"Image not found at path: {img_path}")
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+template  = cv.imread(template_path, 0)
+if template is None: raise FileNotFoundError(f"Template image not found at path {template_path}")
+h_temp, w_temp = template.shape
 
-cv.imshow('gray_test', gray)
-cv.waitKey(0)
-cv.destroyAllWindows()
+matching_methods = [cv.TM_CCOEFF, cv.TM_CCOEFF_NORMED, cv.TM_CCORR, cv.TM_CCOEFF_NORMED, cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]
+
+for method in matching_methods:
+    img_copy = img.copy()
+    result = cv.matchTemplate(img_copy, template, method)
+    min_val , max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        location = min_loc
+    else:
+        location = max_loc
+
+    bottom_right = (location[0] + w_temp, location[1]+ h_temp)
+    cv.rectangle(img_copy, location, bottom_right, (0,255,0), 2)
+    cv.imshow('Matched template', img_copy)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
 quit()
