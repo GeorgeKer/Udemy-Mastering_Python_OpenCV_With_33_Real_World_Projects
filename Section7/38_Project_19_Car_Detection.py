92bb6a6e50f1b297ce419e1418bfaeeab395689e
GeorgeKer <geokeratz@gmail.com>
2026-03-28 17:01:16 +0200

    Bug fixes for cv.rectangle

--- Section7/38_Project_19_Car_Detection.py
+++ Section7/38_Project_19_Car_Detection.py
@@ -3,16 +3,19 @@ import os
 
 VIDEO_PATH = os.path.expanduser('~/INPUT/TEST_VIDEO_cars.mp4')
 cars_cascade = cv.CascadeClassifier('../cars_cascade.xml')
-video_cap = cv.VideoCapture('cars.mp4')
+video_cap = cv.VideoCapture(VIDEO_PATH)
 
 while True:
     ret, frame = video_cap.read()
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    if not ret: break
+    if frame is None: break
     
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     cars = cars_cascade.detectMultiScale(gray, 1.1, 1)
+
     if len(cars) > 0:
         for (x, y, w, h) in cars:
-            cv.rectangle(img=frame, (x,y), (x+w, y+h), (255, 255, 255), 1) # type: ignore
+            cv.rectangle(frame, (x,y), (x+w, y+h), (255, 255, 255), 1) # type: ignore
 
     cv.imshow('Car Detection', frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
